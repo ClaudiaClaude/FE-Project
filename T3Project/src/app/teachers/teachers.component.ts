7f3b48cc9bdbb8f67e5a1ecec23a54937d4dcc15
Claudia Tomassetti <hagol.cker@gmail.com>
2021-07-19 19:38:44 +0200

    adds basic layout for teachers section

--- T3Project/src/app/teachers/teachers.component.ts
+++ T3Project/src/app/teachers/teachers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Teacher } from 'src/course_data';
 
 @Component({
   selector: 'app-teachers',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeachersComponent implements OnInit {
 
+  teachers = Teacher;
   constructor() { }
 
   ngOnInit(): void {

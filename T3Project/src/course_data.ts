a00fc6423020163fdd5d66d140a956c92ade2dab
deiVodda <christoph.schufer@hotmail.com>
2021-07-20 11:12:20 +0200

    all components in app.html and inserted data

--- T3Project/src/course_data.ts
+++ T3Project/src/course_data.ts
@@ -1,56 +1,89 @@
 export const Course = [
-    {
-        name : "Full Stack",
-        description : "",
-        video : "",
-        techtxt : "",
-        tech : [],
-        techlogo : [],
-        offer_name : ["Basic","intermediat","advance"],
-        offer_price: [1000,2000,3000],
-        offer_link: ["www.codefactory.com","www.codefactory.com","www.codefactory.com"]
-    },{
-        name : "Python",
-        description : "",
-        video : "",
-        techtxt : "",
-        tech : [],
-        techlogo : [],
-        offer_name : ["Basic","intermediat","advance"],
-        offer_price: [1000,2000,3000],
-        offer_link: ["www.codefactory.com","www.codefactory.com","www.codefactory.com"]
-    },
-    {
-        name : "Softwar Testing",
-        description : "",
-        video : "",
-        techtxt : "",
-        tech : [],
-        techlogo : [],
-        offer_name : ["Basic","intermediat","advance"],
-        offer_price: [1000,2000,3000],
-        offer_link: ["www.codefactory.com","www.codefactory.com","www.codefactory.com"]
-    }
-]
-
+  {
+    name: 'Full Stack',
+    description: '',
+    video: '',
+    techtxt: '',
+    tech: [],
+    techlogo: [],
+    offer_name: ['Basic', 'intermediat', 'advance'],
+    offer_price: [1000, 2000, 3000],
+    offer_link: [
+      'www.codefactory.com',
+      'www.codefactory.com',
+      'www.codefactory.com',
+    ],
+  },
+  {
+    name: 'Python',
+    description: '',
+    video: '',
+    techtxt: '',
+    tech: [],
+    techlogo: [],
+    offer_name: ['Basic', 'intermediat', 'advance'],
+    offer_price: [1000, 2000, 3000],
+    offer_link: [
+      'www.codefactory.com',
+      'www.codefactory.com',
+      'www.codefactory.com',
+    ],
+  },
+  {
+    name: 'Softwar Testing',
+    description: '',
+    video: '',
+    techtxt: '',
+    tech: [],
+    techlogo: [],
+    offer_name: ['Basic', 'intermediat', 'advance'],
+    offer_price: [1000, 2000, 3000],
+    offer_link: [
+      'www.codefactory.com',
+      'www.codefactory.com',
+      'www.codefactory.com',
+    ],
+  },
+];
 
 export const Teacher = [
-    {
-        fName: "",
-        lName: "",
-        img: "",
-        spec: ""
-    }
-]
+  {
+    fName: 'Jane',
+    lName: 'Doe',
+    img: 'https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814049_960_720.png',
+    spec: 'Our  best web-development expert',
+  },
+  {
+    fName: 'Jack',
+    lName: 'Rossi',
+    img: 'https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814049_960_720.png',
+    spec: 'Our other best web-development expert',
+  },
+  {
+    fName: 'Julie',
+    lName: 'Mustermann',
+    img: 'https://cdn.pixabay.com/photo/2018/11/13/21/43/instagram-3814049_960_720.png',
+    spec: 'Our newer web-development expert',
+  },
+];
 
 export const News = [
-    {
-        img: "",
-        title: "",
-        txt: "",
-        link: ""
-    }
-]
-
-
-
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.dribbble.com%2Fusers%2F115601%2Fscreenshots%2F3280078%2Fcamera-photo-cion-transition.gif&f=1&nofb=1',
+    title: 'Dummy Title whatever whatever',
+    txt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur velit exercitationem omnis, vitae necessitatibus placeat doloremque laborum quisquam cumque dolore est molestiae ex eum dolor aliquam officia odit, amet mollitia!',
+    link: '#',
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.dribbble.com%2Fusers%2F115601%2Fscreenshots%2F3280078%2Fcamera-photo-cion-transition.gif&f=1&nofb=1',
+    title: 'Dummy Title yes they are different',
+    txt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur velit exercitationem omnis, vitae necessitatibus placeat doloremque laborum quisquam cumque dolore est molestiae ex eum dolor aliquam officia odit, amet mollitia!',
+    link: '#',
+  },
+  {
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.dribbble.com%2Fusers%2F115601%2Fscreenshots%2F3280078%2Fcamera-photo-cion-transition.gif&f=1&nofb=1',
+    title: "Dummy Title it's not that",
+    txt: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur velit exercitationem omnis, vitae necessitatibus placeat doloremque laborum quisquam cumque dolore est molestiae ex eum dolor aliquam officia odit, amet mollitia!',
+    link: '#',
+  },
+];
